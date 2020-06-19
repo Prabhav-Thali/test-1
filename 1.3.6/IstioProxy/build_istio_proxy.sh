360bd3b7e57a116cfed8adfde3d9b8be42c023fc
vibhutisawant <41043754+vibhutisawant@users.noreply.github.com>
2020-06-19 13:57:04 +0530

    Update build_istio_proxy.sh

--- 1.3.6/IstioProxy/build_istio_proxy.sh
+++ 1.3.6/IstioProxy/build_istio_proxy.sh
@@ -12,7 +12,7 @@ set -e -o pipefail
 PACKAGE_NAME="Istio Proxy"
 PACKAGE_VERSION="1.3.6"
 CURDIR="$(pwd)"
-REPO_URL="https://raw.githubusercontent.com/vibhutisawant/test/master/1.3.6/patch/"
+REPO_URL="https://raw.githubusercontent.com/vibhutisawant/test/master/1.3.6/patch"
 
 ISTIO_PROXY_REPO_URL="https://github.com/istio/proxy.git"
 LOG_FILE="$CURDIR/logs/${PACKAGE_NAME}-${PACKAGE_VERSION}-$(date +"%F-%T").log"
