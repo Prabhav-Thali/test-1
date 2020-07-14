4ba103f8c72af829c47ae688d1cfb32fed8209a8
vibhutisawant <41043754+vibhutisawant@users.noreply.github.com>
2020-07-15 00:08:39 +0530

    Update build_sonarqube.sh

--- build_sonarqube.sh
+++ build_sonarqube.sh
@@ -10,7 +10,7 @@ set -e -o pipefail
 
 PACKAGE_NAME="sonarqube"
 PACKAGE_VERSION="8.4.0.35506"
-SCANNER_VERSION="4.3.0.2170"
+SCANNER_VERSION="4.4.0.2170"
 
 SOURCE_ROOT="$(pwd)"
 BUILD_DIR="/usr/local"
