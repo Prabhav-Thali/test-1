311f350ebfb90eac4cef299c9627eb16deddd0d0
vibhutisawant <41043754+vibhutisawant@users.noreply.github.com>
2020-11-12 16:05:47 +0530

    Update build_python3.sh

--- python/build_python3.sh
+++ python/build_python3.sh
@@ -139,7 +139,9 @@ function runTest() {
 EOF
 else
                         cat > expected_failures.log << EOF
-    test_decimal test_io
+    test_decimal test_generators test_io test_multiprocessing_fork
+    test_multiprocessing_forkserver test_multiprocessing_spawn
+    test_pdb test_regrtest test_signal test_threading
     test_site
 EOF
 fi
