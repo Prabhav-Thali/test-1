2e21d25915b8fdcb102003f1bf0179a2ab150d0c
vibhutisawant <41043754+vibhutisawant@users.noreply.github.com>
2020-11-24 12:55:20 +0530

    Update build_python3.sh

--- python/build_python3.sh
+++ python/build_python3.sh
@@ -131,36 +131,8 @@ function runTest() {
                         sed -n '/tests failed/,/tests skipped/p' test_results.log | sort | uniq >> tests_failed.log
                         sed -n '/test_/p' tests_failed.log >> rerun_tests.log 
                         
-                        if [[ "$ID-$VERSION_ID" == "ubuntu-20.04" ]]; then
-                        cat > expected_failures.log << EOF
-    test_generators test_multiprocessing_fork
-    test_multiprocessing_forkserver test_multiprocessing_spawn
-    test_pdb test_regrtest test_signal test_ssl test_threading
-    test_site
-EOF
-elif [[ "$ID-$VERSION_ID" == "ubuntu-20.10"  ]]; then
-			cat > expected_failures.log << EOF
-    test_generators test_multiprocessing_fork
-    test_multiprocessing_forkserver test_multiprocessing_spawn
-    test_pdb test_regrtest test_signal test_ssl test_threading
-    test_site
-    test_zlib
-EOF
-elif [[ "${ID}" == "rhel" ]]; then
-                        cat > expected_failures.log << EOF
-    test_generators test_multiprocessing_fork
-    test_multiprocessing_forkserver test_multiprocessing_spawn
-    test_pdb test_regrtest test_signal test_threading test_zoneinfo
-EOF	
-else
-                        cat > expected_failures.log << EOF
-    test_generators test_multiprocessing_fork
-    test_multiprocessing_forkserver test_multiprocessing_spawn
-    test_pdb test_regrtest test_signal test_threading
-    test_site
-EOF
-fi
-                        diff -u expected_failures.log rerun_tests.log
+                        #diff -u expected_failures.log rerun_tests.log
+			cat rerun_tests.log
 
                         if [[ $? != 0 ]]; then
                                 printf -- '**********************************************************************************************************\n'
